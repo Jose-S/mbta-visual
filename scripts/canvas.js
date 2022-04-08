43d6b96fa5760a2f005edd59cd6faa097515be26
Jose Saravia <jose.sarvia99@gmail.com>
2022-04-08 16:00:45 -0400

    Added Netlify Functions

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -7,52 +7,52 @@ var ctx; // Canvas Context
 
 // INIT ARTWORK
 function init() {
-   // GLOBLA VARIABLE SETUP
-   background = document.getElementById("map");
-   background.src = this.getBostonMap();
+    // GLOBLA VARIABLE SETUP
+    background = document.getElementById("map");
+    background.src = this.getBostonMap();
 
-   c = document.getElementById("layer2");
-   ctx = c.getContext("2d");
+    c = document.getElementById("layer2");
+    ctx = c.getContext("2d");
 }
 
 // PARAMS: (X, Y) Point Cord object and a color string
 // EFFECT: Draws the given point on the canvas
 function drawPoint(point, color = "white") {
-   console.log("DRAWING", color);
-   ctx.fillStyle = "#" + color;
-   ctx.fillRect(point.x, point.y, 5, 5);
+    console.log("DRAWING", color);
+    ctx.fillStyle = "#" + color;
+    ctx.fillRect(point.x, point.y, 5, 5);
 }
 
 
 // PARAMS: (X, Y) Point Cord object and a color string
 // EFFECT: Draws the given point on the canvas
 function drawLine(x1, y1, x2, y2, ratio, color) {
-   // Setup Stroke
-   ctx.strokeStyle = "#" + color;
-   ctx.lineCap = "round";
-   c.lineJoin = "round";
-   ctx.lineWidth = STROKE_WIDTH;
-   // Draw Line
-   ctx.beginPath();
-   ctx.moveTo(x1, y1);
-   x2 = x1 + ratio * (x2 - x1);
-   y2 = y1 + ratio * (y2 - y1);
-   ctx.lineTo(x2, y2);
-   ctx.stroke();
+    // Setup Stroke
+    ctx.strokeStyle = "#" + color;
+    ctx.lineCap = "round";
+    c.lineJoin = "round";
+    ctx.lineWidth = STROKE_WIDTH;
+    // Draw Line
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    x2 = x1 + ratio * (x2 - x1);
+    y2 = y1 + ratio * (y2 - y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
 }
 
 // PARAMS: (X, Y) Point Cord object and a color string
 // EFFECT: Draws the given point on the canvas
 function animate(cord1, cord2, ratio, color) {
-   // Draw over the whole canvas to create the trail effect
-   ratio = ratio || 0;
-   drawLine(cord1.x, cord1.y, cord2.x, cord2.y, ratio, color);
-   if (ratio < 1) {
-      requestAnimationFrame(function () {
-         // Draw next line section
-         animate(cord1, cord2, ratio + 0.005, color);
-      });
-   }
+    // Draw over the whole canvas to create the trail effect
+    ratio = ratio || 0;
+    drawLine(cord1.x, cord1.y, cord2.x, cord2.y, ratio, color);
+    if (ratio < 1) {
+        requestAnimationFrame(function() {
+            // Draw next line section
+            animate(cord1, cord2, ratio + 0.005, color);
+        });
+    }
 }
 
 
@@ -63,43 +63,43 @@ function animate(cord1, cord2, ratio, color) {
 // FUTURE NOTE: Map created dynamically to allow location to be changed 
 function getBostonMap() {
 
-   var latlon = CENTER_CORD.lat + "," + CENTER_CORD.long;
+    var latlon = CENTER_CORD.lat + "," + CENTER_CORD.long;
 
-   if (process.env.GOOGLE_MAPS_API_KEY != "") {
-      var img_url = "https://maps.googleapis.com/maps/api/" +
-         // Center Point
-         "staticmap?center=" + latlon +
-         // Zoom and Size
-         "&zoom=14&size=600x600&sensor=false" +
-         // Basic Features 
-         "&style=feature:water|color:0x303030" +
-         "&style=feature:landscape|color:0x000000" +
-         "&style=feature:road|visibility:simplified" +
-         "&style=feature:all|element:labels|visibility:off" +
-         // Raod and Transit Network
-         "&style=feature:road.local|color:0x3A3A3A|weight:1" +
-         "&style=feature:road.arterial|color:0x4E4E4E|weight:2" +
-         "&style=feature:road.highway|visibility:off" +
-         "&style=feature:transit|visibility:off" +
-         // Remove Points of Interest
-         "&style=feature:poi|visibility:off" +
-         "&key=" + process.env.GOOGLE_MAPS_API_KEY;
-   } else {
-      console.log("Asset is being used")
-      var img_url = "assets/bostonMap.png";
-   }
+    try {
+        var img_url = "https://maps.googleapis.com/maps/api/" +
+            // Center Point
+            "staticmap?center=" + latlon +
+            // Zoom and Size
+            "&zoom=14&size=600x600&sensor=false" +
+            // Basic Features 
+            "&style=feature:water|color:0x303030" +
+            "&style=feature:landscape|color:0x000000" +
+            "&style=feature:road|visibility:simplified" +
+            "&style=feature:all|element:labels|visibility:off" +
+            // Raod and Transit Network
+            "&style=feature:road.local|color:0x3A3A3A|weight:1" +
+            "&style=feature:road.arterial|color:0x4E4E4E|weight:2" +
+            "&style=feature:road.highway|visibility:off" +
+            "&style=feature:transit|visibility:off" +
+            // Remove Points of Interest
+            "&style=feature:poi|visibility:off" +
+            "&key=" + process.env.GOOGLE_MAPS_API_KEY;
+    } catch (error) {
+        console.log("Asset is being used")
+        var img_url = "assets/bostonMap.png";
+    }
 
-   return img_url;
+    return img_url;
 }
 
 
 // PARAMS: A PolyLine (String) and Color (String) value 
 // EFFECT: Draws the given polyline on the canvas
 function drawPoly(poly, color) {
-   var d = polyline.decode(poly);
-   d.forEach(cord => {
-      drawPoint(toPixelCord(cord[0], cord[1]), color);
-   })
+    var d = polyline.decode(poly);
+    d.forEach(cord => {
+        drawPoint(toPixelCord(cord[0], cord[1]), color);
+    })
 }
 
 init()
--- scripts/network.js
+++ scripts/network.js
@@ -1,6 +1,8 @@
+const { handler } = require("../netlify/functions/mbta-token-hider/mbta-token-hider");
+
 const APIKEY = process.env.MBTA_API_KEY;
 const KEY_ATTRIBUTE = "&api_key=" + APIKEY;
-var AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
+var AsyncFunction = Object.getPrototypeOf(async function() {}).constructor;
 
 // END POINTS
 //Endpoint to retrieve Route Information
@@ -13,100 +15,100 @@ var liveVehiclesEnpoint = "https://api-v3.mbta.com/vehicles/?api_key=" + APIKEY;
 // PARAMS: URL ednpoint and callback function
 // EFFECT: Fetches endpoint data and runs callback afer fetch is complete
 function fetchMbtaData(endpoint, fetchComplete) {
-   var request = new XMLHttpRequest();
-   request.addEventListener("progress", updateLoadBar);
-   request.addEventListener("load", fetchComplete);
-   request.addEventListener("error", fetchFailed);
-   request.addEventListener("abort", fetchCanceled);
-   request.open('GET', endpoint + KEY_ATTRIBUTE, true);
-   request.send();
+    var request = new XMLHttpRequest();
+    request.addEventListener("progress", updateLoadBar);
+    request.addEventListener("load", fetchComplete);
+    request.addEventListener("error", fetchFailed);
+    request.addEventListener("abort", fetchCanceled);
+    request.open('GET', endpoint + KEY_ATTRIBUTE, true);
+    request.send();
 }
 
 // PARAMS: RequestEvent
 // EFFECT: Logs percent of data fetched
 // NOTE: Can be used to show load bar
 function updateLoadBar(event) {
-   if (event.lengthComputable) {
-      var percentComplete = event.loaded / event.total * 100;
-      console.log("FETCHING" + percentComplete + "%");
-   } else {
-      console.log("FETCHING");
-   }
+    if (event.lengthComputable) {
+        var percentComplete = event.loaded / event.total * 100;
+        console.log("FETCHING" + percentComplete + "%");
+    } else {
+        console.log("FETCHING");
+    }
 }
 
 // PARAMS: RequestEvent
 // EFFECT: Logs data fetch error
 // TODO: Alert Viewer
 function fetchFailed(event) {
-   console.log("FAIL: " + event);
+    console.log("FAIL: " + event);
 }
 
 // PARAMS: RequestEvent
 // EFFECT: Logs data fetch canceled
 // TODO: Alert Viewer
 function fetchCanceled(event) {
-   console.log("CANCELED: " + event);
+    console.log("CANCELED: " + event);
 }
 
 // PARAMS: RequestEvent
 // EFFECT: Collects route data json and creates route objects
 // Creates route objects from event data and fetches route polylines (shapes)
 function fetchRoutes(event) {
-   var status = event.currentTarget.status;
-   var data = JSON.parse(this.response);
-   if (status >= 200 && status < 400) {
+    var status = event.currentTarget.status;
+    var data = JSON.parse(this.response);
+    if (status >= 200 && status < 400) {
 
-      // Array of Routes
-      var routes = data.data;
-      console.log("ROUTES: " + routes);
-      var routesProcessed = 0;
-      routes.forEach(route => {
-         // console.log(route.id);
-         jsonToRoute(route);
-      })
-      console.log('ROUTES PROCESSESED');
-      addRoutePolys();
-   } else {
-      console.log("ERROR NO ROUTE DATA FOUND");
-   }
+        // Array of Routes
+        var routes = data.data;
+        console.log("ROUTES: " + routes);
+        var routesProcessed = 0;
+        routes.forEach(route => {
+            // console.log(route.id);
+            jsonToRoute(route);
+        })
+        console.log('ROUTES PROCESSESED');
+        addRoutePolys();
+    } else {
+        console.log("ERROR NO ROUTE DATA FOUND");
+    }
 }
 
 // PARAMS: RequestEvent
 // EFFECT: Collects vecihle data json and creates veichle objects
 // Creates Veichle objects from event data
 function fetchVeichles(event) {
-   var status = event.currentTarget.status;
-   var data = JSON.parse(this.response);
-   if (status >= 200 && status < 400) {
-      // Array of Routes
-      var veichles = data.data;
-      // console.log("ROUTES: " + veichles);
-      veichles.forEach(veichle => {
-         // console.log(veichle.id);
-         jsonToVeichle(veichle);
-      })
-      console.log('VEICHLES PROCESSESED');
-      // drawAllVeichles();
-   } else {
-      console.log("ERROR NO VEICHLE DATA FOUND");
-   }
+    var status = event.currentTarget.status;
+    var data = JSON.parse(this.response);
+    if (status >= 200 && status < 400) {
+        // Array of Routes
+        var veichles = data.data;
+        // console.log("ROUTES: " + veichles);
+        veichles.forEach(veichle => {
+            // console.log(veichle.id);
+            jsonToVeichle(veichle);
+        })
+        console.log('VEICHLES PROCESSESED');
+        // drawAllVeichles();
+    } else {
+        console.log("ERROR NO VEICHLE DATA FOUND");
+    }
 }
 
 // PARAMS: RequestEvent
 // EFFECT: Collects veichle data json and creates veichle objects
 // Creates Veichle objects from live event data
 function processLiveVeichles(event) {
-   var veichles = JSON.parse(event.data);
-   // var veichles = data.data;
-   // console.log(veichles);
-   if (veichles.length === 0) {
-      console.log("ERROR NO VEICHLE DATA FOUND");
-   }
-   veichles.forEach(veichle => {
-      jsonToVeichle(veichle);
-   })
-   console.log('VEICHLES PROCESSESED');
-   drawAllVeichles();
+    var veichles = JSON.parse(event.data);
+    // var veichles = data.data;
+    // console.log(veichles);
+    if (veichles.length === 0) {
+        console.log("ERROR NO VEICHLE DATA FOUND");
+    }
+    veichles.forEach(veichle => {
+        jsonToVeichle(veichle);
+    })
+    console.log('VEICHLES PROCESSESED');
+    drawAllVeichles();
 
 }
 
@@ -115,88 +117,88 @@ function processLiveVeichles(event) {
 // Initiates live veichle fetch
 // NOTE: Polyline could be used to improve the route of updated veichles 
 function fetchShape(event) {
-   var status = event.currentTarget.status;
-   var data = JSON.parse(this.response);
-   if (status >= 200 && status < 400) {
+    var status = event.currentTarget.status;
+    var data = JSON.parse(this.response);
+    if (status >= 200 && status < 400) {
 
-      // Array of Routes
-      var shapes = data.data;
-      shapes.forEach(shape => {
-         let routeId = shape.relationships.route.data.id;
-         let poly = shape.attributes.polyline;
-         
-         if(allRoutes[routeId]){
-            allRoutes[routeId].updatePoly(poly);
-         }
-         
-      })
-      console.log('POLYS PROCESSESED');
-      // Start fetching veichle data
-      fetchLiveVeichles.startEvent();
-      // fetchMbtaData(veichleEnpoint, fetchVeichles);
-      // drawAllRoutes();
-   } else {
-      console.log("ERROR NO POLYLINE SHAPE DATA FOUND");
-   }
+        // Array of Routes
+        var shapes = data.data;
+        shapes.forEach(shape => {
+            let routeId = shape.relationships.route.data.id;
+            let poly = shape.attributes.polyline;
+
+            if (allRoutes[routeId]) {
+                allRoutes[routeId].updatePoly(poly);
+            }
+
+        })
+        console.log('POLYS PROCESSESED');
+        // Start fetching veichle data
+        fetchLiveVeichles.startEvent();
+        // fetchMbtaData(veichleEnpoint, fetchVeichles);
+        // drawAllRoutes();
+    } else {
+        console.log("ERROR NO POLYLINE SHAPE DATA FOUND");
+    }
 }
 
 // PARAMS: None
 // EFFECT: Creates shapae fetch URL and fetches shape data
 function addRoutePolys() {
-   for (var id in allRoutes) {
-      shapeEnpoint += id + ",";
-   };
-   fetchMbtaData(shapeEnpoint, fetchShape);
+    for (var id in allRoutes) {
+        shapeEnpoint += id + ",";
+    };
+    fetchMbtaData(shapeEnpoint, fetchShape);
 }
 
 // Varaible holsd code for fetching live veichle data using an
 // event stream.
 const fetchLiveVeichles = {
 
-   startEvent: () => {
-      this.eventSource = new EventSource(liveVehiclesEnpoint);
+    startEvent: () => {
+        this.eventSource = new EventSource(liveVehiclesEnpoint);
 
-      this.eventSource.addEventListener("reset", e => {
-         processLiveVeichles(e);
-      });
+        this.eventSource.addEventListener("reset", e => {
+            processLiveVeichles(e);
+        });
 
-      this.eventSource.addEventListener("add", e => {
-         var data = JSON.parse(e.data);
-         console.log("ADDING: " + data.id);
-         var veichle = allVeichles[data.id];
-         veichle != undefined ?
-            veichle.update(data) :
-            jsonToVeichle(data);
-      });
+        this.eventSource.addEventListener("add", e => {
+            var data = JSON.parse(e.data);
+            console.log("ADDING: " + data.id);
+            var veichle = allVeichles[data.id];
+            veichle != undefined ?
+                veichle.update(data) :
+                jsonToVeichle(data);
+        });
 
-      this.eventSource.addEventListener("update", e => {
-         var data = JSON.parse(e.data);
-         console.log("UPDATING: " + data.id);
-         var veichle = allVeichles[data.id];
-         veichle != undefined ?
-            veichle.update(data) :
-            jsonToVeichle(data);
-      });
+        this.eventSource.addEventListener("update", e => {
+            var data = JSON.parse(e.data);
+            console.log("UPDATING: " + data.id);
+            var veichle = allVeichles[data.id];
+            veichle != undefined ?
+                veichle.update(data) :
+                jsonToVeichle(data);
+        });
 
-      this.eventSource.addEventListener("remove", e => {
-         var data = JSON.parse(e.data);
-         console.log("REMOVING: " + data.id);
-         var veichle = allVeichles[data.id];
-         veichle != undefined ?
-            delete veichle :
-            console.log("NOTHING TO REMOVE");
-      });
+        this.eventSource.addEventListener("remove", e => {
+            var data = JSON.parse(e.data);
+            console.log("REMOVING: " + data.id);
+            var veichle = allVeichles[data.id];
+            veichle != undefined ?
+                delete veichle :
+                console.log("NOTHING TO REMOVE");
+        });
 
-      this.eventSource.addEventListener('open', (e) => {
-         console.log('OPEN');
-         console.log(e);
-      });
+        this.eventSource.addEventListener('open', (e) => {
+            console.log('OPEN');
+            console.log(e);
+        });
 
-      this.eventSource.addEventListener('error', (e) => {
-         console.log('got an error');
-         console.log(e);
-      });
-   }
+        this.eventSource.addEventListener('error', (e) => {
+            console.log('got an error');
+            console.log(e);
+        });
+    }
 }
 
 // FOR TESTING
@@ -204,3 +206,5 @@ const fetchLiveVeichles = {
 
 // Begin Fetching Data
 fetchMbtaData(routeEnpoint, fetchRoutes);
+
+handler("get")
